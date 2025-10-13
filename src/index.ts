fa234402e74ecdc961369686cda0ea41218f4e64
leji-a <toviya3135@gmail.com>
2025-10-13 00:16:39 -0300

    category routes and plugin added

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 import { Elysia } from "elysia";
-import { ProductRoutes } from "./models/product/routes";
+import 'dotenv/config'
+import { SupabasePlugin } from "./plugins/supabase"
+import { ProductRoutes } from "./models/product/routes"
+import { CategoryRoutes } from "./models/category/routes"
+
 const app = new Elysia()
+    .use(SupabasePlugin)
+
     .use(ProductRoutes)
+    .use(CategoryRoutes)
     .listen(3000)
 
 console.log(`🛒 Inventory API running at http://${app.server?.hostname}:${app.server?.port}`)
