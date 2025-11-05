7b5b0dc73ccc5203ff9f3fe3c96cc3c1c6410dce
leji-a <toviya3135@gmail.com>
2025-11-05 16:52:22 -0300

    quantity made optional in productOutput

--- src/models/product/schema.ts
+++ src/models/product/schema.ts
@@ -1,11 +1,11 @@
 // product/schema.ts
-import { t } from 'elysia'
+import { t } from "elysia";
 
 export const ProductImageSchema = t.Object({
   id: t.Number(),
   image_url: t.String(),
   display_order: t.Number(),
-})
+});
 
 // For incoming requests (create/update)
 export const ProductInputSchema = t.Object({
@@ -13,18 +13,19 @@ export const ProductInputSchema = t.Object({
   quantity: t.Optional(t.Number({ minimum: 0 })),
   price: t.Number({ minimum: 0.01 }),
   categoryIds: t.Optional(t.Array(t.Number())),
-})
-// For responses from DB 
+});
+
+// For responses from DB
 export const ProductOutputSchema = t.Object({
   id: t.Number(),
   name: t.String(),
-  quantity: t.Number(),
+  quantity: t.Optional(t.Number()),
   price: t.Number(),
   categoryIds: t.Array(t.Number()),
   categoryNames: t.Array(t.String()),
   created_at: t.Optional(t.String()),
   images: t.Array(ProductImageSchema),
-})
+});
 
 export const PaginationSchema = t.Object({
   page: t.Number(),
@@ -33,17 +34,18 @@ export const PaginationSchema = t.Object({
   totalPages: t.Number(),
   hasNextPage: t.Boolean(),
   hasPrevPage: t.Boolean(),
-})
+});
 
 export const PaginatedProductsSchema = t.Object({
   data: t.Array(ProductOutputSchema),
   pagination: PaginationSchema,
-})
+});
 
 export const ErrorSchema = t.Object({
-  error: t.String()
-})
+  error: t.String(),
+});
 
-export type ProductInput = typeof ProductInputSchema.static
-export type ProductOutput = typeof ProductOutputSchema.static
-export type ProductImage = typeof ProductImageSchema.static
+export type ProductInput = typeof ProductInputSchema.static;
+export type ProductOutput = typeof ProductOutputSchema.static;
+export type ProductImage = typeof ProductImageSchema.static;
+
